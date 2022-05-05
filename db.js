6c21352968f85bb985dd65cd6681c94d4987a1ea
panupongth148 <97428822+panupongth148@users.noreply.github.com>
2022-05-05 07:22:03 +0700

    edit db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // const mysql = require('mysql2/promise')
 const mysql = require('mysql')
-
+require("dotenv").config()
 // const db = mysql.createPool({
 //     host: process.env.HOSTSQL,
 //     port: process.env.PORTSQL,
@@ -12,11 +12,11 @@ const mysql = require('mysql')
 //     queueLimit: 0
 // })
 const db = mysql.createConnection({
-    host:"scheduledb.cdq6womsw7wv.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "admin",
-    password: "9Up7757rAnJVaCZk",
-    database: "dbName",
+    host: process.env.HOSTSQL,
+    port: process.env.PORTSQL,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASENAME
 })
 db.connect((err) =>{
     if(err){
